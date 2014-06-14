a0214bcc23b297d547a536ab520922cb7b3136d9
Derek Brameyer <derek.brameyer@willowtreeapps.com>
2014-06-14 16:58:18 -0400

    Remove ACRA, update pom for latest Android Maven plugin and DashClock API.

--- src/com/doomonafireball/umbee/MainApp.java
+++ src/com/doomonafireball/umbee/MainApp.java
@@ -5,15 +5,11 @@ import com.google.inject.Injector;
 
 import com.littlefluffytoys.littlefluffylocationlibrary.LocationLibrary;
 
-import org.acra.ACRA;
-import org.acra.annotation.ReportsCrashes;
-
 import android.app.Application;
 import android.util.Log;
 
 import roboguice.RoboGuice;
 
-@ReportsCrashes(formKey = "dEhNYllsNE9NbnNnY1lJeVlFcVhzVkE6MQ")
 public class MainApp extends Application {
 
     public static String TAG = "umbee";
@@ -22,7 +18,6 @@ public class MainApp extends Application {
 
     @Override
     public void onCreate() {
-        ACRA.init(this);
         super.onCreate();
         Log.i(TAG, "onCreate");
         LocationLibrary.initialiseLibrary(getBaseContext(), "com.doomonafireball.umbee");
